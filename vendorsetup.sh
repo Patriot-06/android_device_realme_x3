fb584ce71beeac6277f7f0e380700d7f61969822
“Patriot-06” <mbmc172@gmail.com>
2022-05-31 18:45:58 +0530

    x3: Switch to Proton-clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,4 @@ sudo rm -r kernel/realme
 git clone https://github.com/Patriot-06/hardware_oplus.git hardware/oplus
 git clone https://github.com/Patriot-06/android_vendor_realme_x3.git vendor/realme/x3
 git clone https://github.com/Patriot-06/android_kernel_realme_x3.git kernel/realme/x3
-git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git prebuilts/clang/host/linux-x86/clang-azure
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
